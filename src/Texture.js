e0481ab89805e7b862c4674afd31e7b789603b0d
Mstislav Zhivodkov <m.zhivodkov@2Gis.ru>
2015-07-27 19:03:26 +0600

    try add texture

---
+++ src/Texture.js
@@ -0,0 +1,33 @@
+export default class Texture {
+    constructor(src) {
+        this._src = src;
+    }
+
+    enable(gl, uniform) {
+        if (!this._glBuffer) {
+            this._prepare(gl);
+        }
+
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, this._texture);
+        gl.uniform1i(uniform, 0);
+
+        return this;
+    }
+
+    disable(gl) {
+
+        return this;
+    }
+
+    _prepare(gl) {
+        this._texture = gl.createTexture();
+
+        gl.bindTexture(gl.TEXTURE_2D, this._texture);
+        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this._src);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.bindTexture(gl.TEXTURE_2D, null);
+    }
+}

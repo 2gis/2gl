87735e3db56e049004dd3f735f6e1a54e03050c5
Mstislav Zhivodkov <m.zhivodkov@2Gis.ru>
2015-08-04 13:42:06 +0600

    add renderer clearcolor

--- src/Renderer.js
+++ src/Renderer.js
@@ -1,7 +1,10 @@
 export default class Renderer {
     constructor(options) {
         this._container = document.getElementById(options.container);
-        this._pixelRatio = 1;
+        this._pixelRatio = options.pixelRatio || 1;
+        this._antialias = options.antialias !== undefined ? options.antialias : true;
+        this.autoClear = options.autoClear !== undefined ? options.autoClear : true;
+        this.clearColor = options.clearColor || [1, 1, 1, 1];
 
         this._initCanvas();
     }
@@ -22,13 +25,24 @@ export default class Renderer {
         return this;
     }
 
+    clear() {
+        let gl = this._gl;
+
+        gl.clearColor.apply(gl, this.clearColor);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        return this;
+    }
+
     render(scene, camera) {
         let gl = this._gl;
 
-        gl.clearColor(1, 1, 1, 1);
         gl.enable(gl.DEPTH_TEST);
         gl.enable(gl.CULL_FACE);
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        if (this.autoClear) {
+            this.clear();
+        }
 
         camera.updateLocalMatrix();
         camera.updateWorldMatrix();
@@ -46,6 +60,10 @@ export default class Renderer {
 
         this._container.appendChild(this._canvasElement);
 
-        this._gl = this._canvasElement.getContext('webgl');
+        let attributes = {
+            antialias: this._antialias
+        };
+
+        this._gl = this._canvasElement.getContext('webgl', attributes);
     }
 }

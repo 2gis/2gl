681a88108149494c0de6bfb16a9e2786e7a473a6
Mstislav Zhivodkov <m.zhivodkov@2Gis.ru>
2015-09-01 17:18:40 +0600

    fix sprite frag shader

--- src/shaders/sprite.frag.glsl
+++ src/shaders/sprite.frag.glsl
@@ -8,5 +8,5 @@ varying vec2 vTextureCoord;
 void main(void) {
     vec4 textureColor = texture2D(uTexture, vec2(vTextureCoord.s, vTextureCoord.t));
 
-    gl_FragColor = vec4(textureColor.rgb, uColorAlpha);
+    gl_FragColor = vec4(textureColor.rgb, textureColor.a * uColorAlpha);
 }

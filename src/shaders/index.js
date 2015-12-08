0c971e29c7b2fdc5f4cb6cd7e00cef0872a9950c
Mstislav Zhivodkov <m.zhivodkov@2Gis.ru>
2015-12-08 11:14:47 +0600

    Update babelify and fix error with shaders export

--- src/shaders/index.js
+++ src/shaders/index.js
@@ -4,17 +4,17 @@ var fs = require('fs');
 var path = require('path');
 /* eslint-enable no-var */
 
-export default {
-    basic: {
-        vertex: fs.readFileSync(path.join(__dirname, '/basic.vert.glsl'), 'utf8'),
-        fragment: fs.readFileSync(path.join(__dirname, '/basic.frag.glsl'), 'utf8')
-    },
-    complex: {
-        vertex: fs.readFileSync(path.join(__dirname, '/complex.vert.glsl'), 'utf8'),
-        fragment: fs.readFileSync(path.join(__dirname, '/complex.frag.glsl'), 'utf8')
-    },
-    sprite: {
-        vertex: fs.readFileSync(path.join(__dirname, '/sprite.vert.glsl'), 'utf8'),
-        fragment: fs.readFileSync(path.join(__dirname, '/sprite.frag.glsl'), 'utf8')
-    }
+export const basic = {
+    vertex: fs.readFileSync(path.join(__dirname, '/basic.vert.glsl'), 'utf8'),
+    fragment: fs.readFileSync(path.join(__dirname, '/basic.frag.glsl'), 'utf8')
+};
+
+export const complex = {
+    vertex: fs.readFileSync(path.join(__dirname, '/complex.vert.glsl'), 'utf8'),
+    fragment: fs.readFileSync(path.join(__dirname, '/complex.frag.glsl'), 'utf8')
+};
+
+export const sprite = {
+    vertex: fs.readFileSync(path.join(__dirname, '/sprite.vert.glsl'), 'utf8'),
+    fragment: fs.readFileSync(path.join(__dirname, '/sprite.frag.glsl'), 'utf8')
 };

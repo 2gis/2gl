09289b83a5fa2426a0f9e52f6d9d984388bdd488
Mstislav Zhivodkov <m.zhivodkov@2Gis.ru>
2016-04-29 13:58:27 +0600

    Move math objects to modules

--- src/index.js
+++ src/index.js
@@ -12,13 +12,7 @@ import Mesh from './Mesh';
 import Scene from './Scene';
 import Texture from './Texture';
 import Raycaster from './Raycaster';
-import RenderTarget from './RenderTarget';
-import Ray from './math/Ray';
-import Plane from './math/Plane';
-import Box from './math/Box';
-import Frustum from './math/Frustum';
 import * as math from './math/Math';
-import Line3 from './math/Line3';
 import {vec3, mat3, vec2, mat4, quat, glMatrix} from 'gl-matrix';
 
 // with Float32Array we have errors with raycast
@@ -35,12 +29,6 @@ const dgl = {
     Scene,
     Texture,
     Raycaster,
-    RenderTarget,
-    Ray,
-    Plane,
-    Frustum,
-    Box,
-    Line3,
     Math: math,
     vec3,
     mat3,

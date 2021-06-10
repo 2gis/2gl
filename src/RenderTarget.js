1707e1735963d402b527ec9fc12cecec15107106
Alexander Artemyev <al.artemev@2gis.ru>
2021-06-10 19:12:02 +0700

    patch RenderTarget for heatmap

--- src/RenderTarget.js
+++ src/RenderTarget.js
@@ -15,6 +15,13 @@ class RenderTarget {
         this.options = Object.assign({}, RenderTarget.defaultOptions, options);
 
         /**
+         * Текстура создается в конструкторе, чтобы можно было сразу получить на нее ссылку.
+         * @type {?Texture}
+         * @ignore
+         */
+        this._texture = new Texture(null, this.options);
+
+        /**
          * Контекст WebGL, в котором был инициализирован фреймбуфер.
          * Используется только для удаления, подумать хорошо, прежде чем использовать для чего-то ещё.
          * @type {?WebGLRenderingContext}
@@ -79,7 +86,11 @@ class RenderTarget {
      */
     _prepare(gl) {
         this._glContext = gl;
-        this._texture = new Texture(null, this.options);
+        
+        // Проверяем наличие текстуры, т.к. она может быть удалена через метод _unprepare.
+        if (!this._texture) {
+            this._texture = new Texture(null, this.options);
+        }
         this._texture.prepare(gl);
 
         this._frameBuffer = gl.createFramebuffer();
